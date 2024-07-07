b3a20f87518d5c36b4d90aa446ed1c5b3acaa35a
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2024-07-07 15:33:48 -0700

    Added Tanstack Query Package

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,15 @@ import './index.css';
 import './utils/fontAwesome';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './utils/http.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
